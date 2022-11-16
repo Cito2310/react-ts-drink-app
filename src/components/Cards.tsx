2da577d1ca2e1532ab09921ff310d080c1f18bc8
Cito2310 <cito2310@gmail.com>
2022-11-16 15:34:37 -0300

    Added amount card controllers

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,4 +1,6 @@
-import { useLayoutEffect } from 'react';
+import { useLayoutEffect, useState } from 'react';
+import axios from 'axios';
+
 import { IProduct } from '../interfaces/IResProduct';
 
 import "../style/cards.scss"
@@ -8,6 +10,42 @@ interface props {
 }
 
 export const Cards = ( { product }: props ) => {
+    // AMOUNT CONTROLLER
+    const [amount, setAmount] = useState<number>(product.amount);
+    const [timerAmount, setTimerAmount] = useState<string | boolean>("");
+    
+    const checkTimer = (timerAmount === true || timerAmount === "");
+    const changePlusAmount = () => {
+        if (amount < 50) {
+            setAmount(amount + 1);
+            if (checkTimer) setTimerAmount(false);
+    }}
+
+    const changeSubAmount = () => {
+        if (amount > 0) {
+            setAmount(amount - 1); 
+            if (checkTimer) setTimerAmount(false);
+    }}
+
+    useLayoutEffect(() => {
+        if (timerAmount === false) {setTimeout(()=>{setTimerAmount(true)},1000)}
+        if (timerAmount === true) {axios.put(`https://node-ts-load-drink.onrender.com/api/product/amount/${product._id}`, {newAmount: amount})}
+    }, [timerAmount])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // RENDER
     return (
         <div className='card-container'>
             <div className='container-description'>
@@ -29,9 +67,9 @@ export const Cards = ( { product }: props ) => {
             </div>
 
             <div className='container-button-amount'>
-                <i className="fa-solid fa-chevron-up"/>
-                <p>{product.amount}</p>
-                <i className="fa-solid fa-chevron-down"/>
+                <i className="fa-solid fa-chevron-up" onClick={changePlusAmount}/>
+                <p>{amount}</p>
+                <i className="fa-solid fa-chevron-down" onClick={changeSubAmount}/>
             </div>
         </div>
     )
