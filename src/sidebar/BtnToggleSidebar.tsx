246260d0263e7d62c4848e4eed91e2288a490e76
Cito2310 <cito2310@gmail.com>
2022-11-17 19:30:59 -0300

    Added styled to button toggle sidebar

--- src/sidebar/BtnToggleSidebar.tsx
+++ src/sidebar/BtnToggleSidebar.tsx
@@ -1,11 +1,12 @@
 import { useContext } from 'react';
 import { contextSideBar } from './ProviderSideBarController';
 
+import "./btn-toggle-sidebar.scss"
 
 export const BtnToggleSidebar = () => {
     const {toggleSidebar} = useContext(contextSideBar)
 
     return (
-        <button onClick={toggleSidebar}>Toggle Sidebar</button>
+        <button onClick={toggleSidebar} className="btn-toggle-sidebar"><i className="fa-solid fa-bars"></i></button>
     )
 }
