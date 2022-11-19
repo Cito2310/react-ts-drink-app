5447ede797eab84881a4116e56ff52d8af1f6729
Cito2310 <cito2310@gmail.com>
2022-11-19 19:31:41 -0300

    Added basic styled to sidebar

--- src/sidebar/SideBar.tsx
+++ src/sidebar/SideBar.tsx
@@ -11,14 +11,23 @@ export const SideBar = () => {
         <div className={sidebarOpen ? "sidebar open left" : "sidebar left"}> {/* PARA CAMBIAR EL LADO DONDE SE DESPLEGARA LA BARRA LATERAL CAMBIAR right | left */}
             {/* MODIFICAR DESDE AQUI */}
             <div className='sidebar-section-top'>
-                <h2>Titulo</h2>
+                <h2>DRINK APP</h2>
                 <i onClick={toggleSidebar} className="fa-solid fa-xmark"></i> {/* ESTE ELEMENTO ES REQUERIDO - NO BORRAR */}
             </div>
 
             <div className='sidebar-section-container'>
-                <h3>Texto Uno</h3>
-                <h3>Texto Dos</h3>
-                <h3>Texto Tres</h3>
+
+                <button className='last-button'>Añadir un nuevo producto</button>
+
+                <hr/>
+                <button>Todos los productos</button>
+                <button className='last-button'>Solo productos seleccionados</button>
+
+                <hr/>
+
+                <button>Mostrar cartas</button>
+                <button className='last-button'>Mostrar lista</button>
+
             </div>
             {/* MODIFICAR HASTA AQUI */}
         </div>
