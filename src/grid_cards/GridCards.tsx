5a8d391f2ce3c6d1895dbfda0fabe8f7cee561da
Cito2310 <cito2310@gmail.com>
2022-11-20 19:10:52 -0300

    Change productSection and GridCards

--- src/grid_cards/GridCards.tsx
+++ src/grid_cards/GridCards.tsx
@@ -1,27 +1,16 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-
-import { IRespProduct } from '../interfaces/IResProduct';
+import { IProduct } from '../interfaces/IResProduct';
 import { Cards } from './Cards';
 
 import "./grid-cards-products.scss"
 
-export const GridCards = () => {
-    const [respProduct, setRespProduct] = useState<IRespProduct>({status: false, data: []})
+interface props {
+    products: IProduct[]
+}
 
-    useEffect(() => {
-        const getProduct = async() => {
-            const { data } = await axios.get("https://node-ts-load-drink.onrender.com/api/product")
-            return data;
-        }
-        getProduct()
-            .then( data => {  setRespProduct({ status: true, data })  })
-            .catch(console.error)
-    }, [])
-
+export const GridCards = ({ products } : props) => {
     return (
         <section id='grid-cards-products'>
-            {( respProduct.status ) ? respProduct.data.map(product => <Cards key={product._id} product={product}/>) : <h1>Cargando...</h1> }
+            { products.map(product => <Cards key={product._id} product={product}/>) }
         </section>
     )
 }
--- src/product_section/ProductSection.tsx
+++ src/product_section/ProductSection.tsx
@@ -1,6 +1,25 @@
 import { GridCards } from '../grid_cards/GridCards';
+
+import { IRespProduct } from '../interfaces/IResProduct';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 export const ProductSection = () => {
+    const [respProduct, setRespProduct] = useState<IRespProduct>({status: false, data: []})
+
+    useEffect(() => {
+        const getProduct = async() => {
+            const { data } = await axios.get("https://node-ts-load-drink.onrender.com/api/product")
+            return data;
+        }
+        getProduct()
+            .then( data => {  setRespProduct({ status: true, data })  })
+            .catch(console.error)
+    }, [])
+
     return (
-        <GridCards/>
+        (respProduct.status) 
+        ? <GridCards products={respProduct.data}/>
+        : <p>Cargando...</p>
     )
 }
