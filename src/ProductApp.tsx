692d12e32699b5cda298f457de8f85936ae6ad53
Cito2310 <cito2310@gmail.com>
2022-11-21 10:43:45 -0300

    Remove innecesary imports

--- src/ProductApp.tsx
+++ src/ProductApp.tsx
@@ -2,7 +2,6 @@ import { ProviderSideBar } from './sidebar/ProviderSideBarController';
 
 import { BtnToggleSidebar } from './sidebar/BtnToggleSidebar';
 import { SideBar } from './sidebar/SideBar';
-import { ProductSection } from './product_section/ProductSection';
 
 import "./config.scss"
 import { RoutesProduct } from './product_section/RoutesProduct';
--- src/product_section/RoutesProduct.tsx
+++ src/product_section/RoutesProduct.tsx
@@ -4,8 +4,6 @@ import axios from 'axios';
 
 import { IRespProduct } from '../interfaces/IResProduct';
 
-import { GridCards } from '../grid_cards/GridCards';
-import { GridList } from '../grid_list/GridList';
 import { ProductSection } from './ProductSection';
 
 export const RoutesProduct = () => {
@@ -17,8 +15,7 @@ export const RoutesProduct = () => {
       return data;
     }
     getProduct()
-          // .then( data => {  setRespProduct({ status: true, data })   })
-          .then( data => {  setRespProduct({ status: true, data }); console.log("se activo")   })
+          .then( data => {  setRespProduct({ status: true, data })   })
           .catch(console.error)
   }, [])
 
@@ -31,14 +28,3 @@ export const RoutesProduct = () => {
     </Routes>
   )
 }
-
-
-// export const ProductSection = () => {
-
-//     return (
-//         (respProduct.status) 
-//         // ? <GridCards products={respProduct.data}/>
-//         ? <GridList products={respProduct.data}/>
-//         : <p>Cargando...</p>
-//     )
-// }
